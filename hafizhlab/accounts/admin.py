dfa544a2ccfed325aea6f94ac711ad857f1e16cb
thami.endamora <thamiendamora@gmail.com>
2021-01-29 16:19:19 +0700

    Add user to admin

--- hafizhlab/accounts/admin.py
+++ hafizhlab/accounts/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from hafizhlab.accounts.models import User
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
