7f330b5b956f3ad85b61a66624a1b9d11d8e1a4e
hashlash <muh.ashlah@gmail.com>
2021-01-31 11:05:15 +0700

    Set email console backend

--- hafizhlab/settings.py
+++ hafizhlab/settings.py
@@ -158,3 +158,9 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Email
+# https://docs.djangoproject.com/en/3.1/topics/email/
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
