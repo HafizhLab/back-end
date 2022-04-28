ab81fe94686fdacde8a72048f91e09ca7f8e5f40
hashlash <muh.ashlah@gmail.com>
2022-04-28 15:28:45 +0700

    Rename challenge check to validate

--- hafizhlab/challenges/models.py
+++ hafizhlab/challenges/models.py
@@ -50,15 +50,15 @@ class Question(models.Model):
     options = models.JSONField()
 
     def clean(self):
-        self._check_ayah_in_challenge_scope()
-        self._check_answer_exist()
-        self._check_option_format()
+        self._validate_ayah_in_challenge_scope()
+        self._validate_answer_exist()
+        self._validate_option_format()
 
-    def _check_ayah_in_challenge_scope(self):
+    def _validate_ayah_in_challenge_scope(self):
         if not self.challenge.scope.ayah_set.filter(pk=self.ayah.pk).exists():
             raise ValidationError(_("Question's ayah should be in the scope"))
 
-    def _check_answer_exist(self):
+    def _validate_answer_exist(self):
         error_msg = _('The answer should exist in options')
         if self.challenge.mode == Challenge.Mode.AYAH_BASED:
             if self.ayah_id not in self.options:
@@ -68,13 +68,13 @@ class Question(models.Model):
             if not all(word in option for word, option in zip(self.ayah.text.split(), self.options)):
                 raise ValidationError(error_msg)
 
-    def _check_option_format(self):
+    def _validate_option_format(self):
         option_type = {
             Challenge.Mode.AYAH_BASED: int,
             Challenge.Mode.WORD_BASED: str,
         }
 
-        def check_format(opt):
+        def validate_format(opt):
             return operator.and_(
                 len(opt) == self.NUMBER_OF_OPTIONS,
                 all(isinstance(x, option_type[self.challenge.mode])
@@ -82,13 +82,13 @@ class Question(models.Model):
             )
 
         if self.challenge.mode == Challenge.Mode.AYAH_BASED:
-            if not check_format(self.options):
+            if not validate_format(self.options):
                 raise ValidationError(_(
                     "Question's options for ayah based challenge should be in "
                     "the format of [<ayah_id>, <ayah_id>, <ayah_id>, <ayah_id>]."
                 ))
         elif self.challenge.mode == Challenge.Mode.WORD_BASED:
-            if not all(check_format(o) for o in self.options):
+            if not all(validate_format(o) for o in self.options):
                 raise ValidationError(_(
                     "Question's options for word based challenge should be in "
                     "the format of [['opt1', 'opt2', 'opt3', 'opt4'], "
