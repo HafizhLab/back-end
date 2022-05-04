2913a8dd7a98f65737832b0e384e7ff1dd054e40
hashlash <muh.ashlah@gmail.com>
2022-05-04 08:59:54 +0700

    Remove question inline change permission due to performance issue

--- hafizhlab/challenges/admin.py
+++ hafizhlab/challenges/admin.py
@@ -5,7 +5,17 @@ from hafizhlab.challenges.models import Challenge, Question
 
 class QuestionInline(admin.TabularInline):
     model = Question
+    show_change_link = True
     extra = 0
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Challenge)
